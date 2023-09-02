a2862bb242facfa2808008898ef49f45deae895d
Son Quoc Tran <tran_s2@denison.edu>
2023-09-02 11:20:48 -0400

    Add squad data

--- src/step1/command/convert_format.sh
+++
@@ -1,4 +0,0 @@
-python /Volumes/Share/tran_s2/AGent_Hub/All_code_submission/code/convert_format.py \
-    "/Volumes/Share/tran_s2/Public_Datasets/mrqa/mrqa_train/NQ/naturalQuestionsShort-train-from-MRQA.json" \
-    "/Volumes/Share/tran_s2/AGent_Hub/All_code_submission/Data/NQ/NQ_mrqa.json" \
-    2
